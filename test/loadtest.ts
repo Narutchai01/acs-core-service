c7e8263674665c3ae669f6063d2d7f72a23b4747
Narutchai <koonfluk2015@gmail.com>
2026-02-12 11:51:41 +0700

    fix: reduce virtual users in load test and specify query parameters for student API request

--- test/loadtest.ts
+++ test/loadtest.ts
@@ -2,12 +2,14 @@ import http from "k6/http";
 import { check, sleep } from "k6";
 
 export const options = {
-  vus: 10000,
+  vus: 100,
   duration: "30s",
 };
 
 export default function () {
-  const res = http.get("http://localhost:8000/api/v1/students");
+  const res = http.get(
+    "http://localhost:8000/api/v1/students?page=1&pageSize=10",
+  );
 
   check(res, {
     "status is 200": (r) => r.status === 200,
