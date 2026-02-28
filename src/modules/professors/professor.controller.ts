f07a60b241e35a9828479047ec7df310b02a057a
asnaweevixvify <asnaweeezor@gmail.com>
2026-02-28 22:50:49 +0700

    remove guard from Retrieves a list of professors / Retrieves a professor by ID

--- src/modules/professors/professor.controller.ts
+++ src/modules/professors/professor.controller.ts
@@ -30,136 +30,136 @@ const PERMISSION = {
 
 export const ProfessorController = (app: Elysia) =>
   app.group("/professors", (app) =>
-    app.decorate("professorService", professorService).guard({}, (privateApp) =>
-      privateApp
-        .use(authMiddleware)
-        .use(roleMacro)
-        .post(
-          "",
-          async ({ professorService, body, set }) => {
-            const professor = await professorService.createProfessor(body);
-            if (!professor) {
-              set.status = HttpStatusCode.INTERNAL_SERVER_ERROR;
-              return success(
-                null,
-                "Failed to create professor",
-                HttpStatusCode.INTERNAL_SERVER_ERROR,
-              );
-            }
-            set.status = HttpStatusCode.CREATED;
+    app
+      .decorate("professorService", professorService)
+      .get(
+        "",
+        async ({ professorService, query, set }) => {
+          const professors = await professorService.getProfessors(query);
+          if (!professors) {
+            set.status = HttpStatusCode.INTERNAL_SERVER_ERROR;
             return success(
-              professor,
-              "Professor created successfully",
-              HttpStatusCode.CREATED,
+              null,
+              "Failed to retrieve professors",
+              HttpStatusCode.INTERNAL_SERVER_ERROR,
             );
-          },
-          {
-            ...ProfessorDocs.createProfessor,
-            checkRole: PERMISSION.ADMINPERSMISSION,
-          },
-        )
-        .get(
-          "",
-          async ({ professorService, query, set }) => {
-            const professors = await professorService.getProfessors(query);
-            if (!professors) {
-              set.status = HttpStatusCode.INTERNAL_SERVER_ERROR;
-              return success(
-                null,
-                "Failed to retrieve professors",
-                HttpStatusCode.INTERNAL_SERVER_ERROR,
-              );
-            }
-            set.status = HttpStatusCode.OK;
+          }
+          set.status = HttpStatusCode.OK;
+          return success(
+            professors,
+            "Professors retrieved successfully",
+            HttpStatusCode.OK,
+          );
+        },
+        {
+          ...ProfessorDocs.getProfessors,
+        },
+      )
+      .get(
+        "/:id",
+        async ({ professorService, params, set }) => {
+          const professor = await professorService.getProfessorById(
+            Number(params.id),
+          );
+          if (!professor) {
+            set.status = HttpStatusCode.NOT_FOUND;
             return success(
-              professors,
-              "Professors retrieved successfully",
-              HttpStatusCode.OK,
+              null,
+              "Professor not found",
+              HttpStatusCode.NOT_FOUND,
             );
-          },
-          {
-            ...ProfessorDocs.getProfessors,
-            checkRole: PERMISSION.ADMINPERSMISSION,
-          },
-        )
-        .get(
-          "/:id",
-          async ({ professorService, params, set }) => {
-            const professor = await professorService.getProfessorById(
-              Number(params.id),
-            );
-            if (!professor) {
-              set.status = HttpStatusCode.NOT_FOUND;
+          }
+          set.status = HttpStatusCode.OK;
+          return success(
+            professor,
+            "Professor retrieved successfully",
+            HttpStatusCode.OK,
+          );
+        },
+        {
+          ...ProfessorDocs.getProfessorById,
+        },
+      )
+      .guard({}, (privateApp) =>
+        privateApp
+          .use(authMiddleware)
+          .use(roleMacro)
+          .post(
+            "",
+            async ({ professorService, body, set }) => {
+              const professor = await professorService.createProfessor(body);
+              if (!professor) {
+                set.status = HttpStatusCode.INTERNAL_SERVER_ERROR;
+                return success(
+                  null,
+                  "Failed to create professor",
+                  HttpStatusCode.INTERNAL_SERVER_ERROR,
+                );
+              }
+              set.status = HttpStatusCode.CREATED;
               return success(
-                null,
-                "Professor not found",
-                HttpStatusCode.NOT_FOUND,
+                professor,
+                "Professor created successfully",
+                HttpStatusCode.CREATED,
               );
-            }
-            set.status = HttpStatusCode.OK;
-            return success(
-              professor,
-              "Professor retrieved successfully",
-              HttpStatusCode.OK,
-            );
-          },
-          {
-            ...ProfessorDocs.getProfessorById,
-            checkRole: PERMISSION.ADMINPERSMISSION,
-          },
-        )
-        .patch(
-          "/:id",
-          async ({ professorService, params, body, set }) => {
-            const professor = await professorService.updateProfessor(
-              Number(params.id),
-              body,
-            );
-            if (!professor) {
-              set.status = HttpStatusCode.NOT_FOUND;
+            },
+            {
+              ...ProfessorDocs.createProfessor,
+              checkRole: PERMISSION.ADMINPERSMISSION,
+            },
+          )
+          .patch(
+            "/:id",
+            async ({ professorService, params, body, set }) => {
+              const professor = await professorService.updateProfessor(
+                Number(params.id),
+                body,
+              );
+              if (!professor) {
+                set.status = HttpStatusCode.NOT_FOUND;
+                return success(
+                  null,
+                  "Professor not found",
+                  HttpStatusCode.NOT_FOUND,
+                );
+              }
+              set.status = HttpStatusCode.OK;
               return success(
-                null,
-                "Professor not found",
-                HttpStatusCode.NOT_FOUND,
+                professor,
+                "Professor updated successfully",
+                HttpStatusCode.OK,
               );
-            }
-            set.status = HttpStatusCode.OK;
-            return success(
-              professor,
-              "Professor updated successfully",
-              HttpStatusCode.OK,
-            );
-          },
-          {
-            ...ProfessorDocs.updateProfessor,
-            checkRole: PERMISSION.ADMINPERSMISSION,
-          },
-        )
-        .delete(
-          "/:id",
-          async ({ professorService, params, set }) => {
-            const professor = await professorService.deleteProfessor(
-              Number(params.id),
-            );
-            if (!professor) {
-              set.status = HttpStatusCode.NOT_FOUND;
+            },
+            {
+              ...ProfessorDocs.updateProfessor,
+              checkRole: PERMISSION.ADMINPERSMISSION,
+            },
+          )
+          .delete(
+            "/:id",
+            async ({ professorService, params, set }) => {
+              const professor = await professorService.deleteProfessor(
+                Number(params.id),
+              );
+              if (!professor) {
+                set.status = HttpStatusCode.NOT_FOUND;
+                return success(
+                  null,
+                  "Professor not found",
+                  HttpStatusCode.NOT_FOUND,
+                );
+              }
+              set.status = HttpStatusCode.OK;
               return success(
-                null,
-                "Professor not found",
-                HttpStatusCode.NOT_FOUND,
+                professor,
+                "Professor deleted successfully",
+                HttpStatusCode.OK,
               );
-            }
-            set.status = HttpStatusCode.OK;
-            return success(
-              professor,
-              "Professor deleted successfully",
-              HttpStatusCode.OK,
-            );
-          },
-          {
-            ...ProfessorDocs.deleteProfessor,
-            checkRole: PERMISSION.ADMINPERSMISSION,
-          },
-        ),
-    ),
+            },
+            {
+              ...ProfessorDocs.deleteProfessor,
+              checkRole: PERMISSION.ADMINPERSMISSION,
+            },
+          ),
+      ),
   );
